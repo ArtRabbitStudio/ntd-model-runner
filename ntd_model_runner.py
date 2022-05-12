1ece9033ef745ae5cdc713adeee33d3245ed0af8
Igor Clark <igor@igorclark.net>
2022-05-12 18:12:27 +0100

    transform NTD model results into IHME/IPM outputs

--- ntd_model_runner.py
+++ ntd_model_runner.py
@@ -1,35 +1,129 @@
-from sch_simulation.helsim_RUN_KK import *
-from sch_simulation.helsim_FUNC_KK import *
+import pickle
+import time
+import multiprocessing
+import math
+import os
 
-from joblib import Parallel, delayed
+import pandas
 
 import gcs
 import db
 
-import pickle
-import time
-import multiprocessing
+from joblib import Parallel, delayed
 
-insert_result_sql = '''
-INSERT INTO results( Time, age_start, age_end, intensity, measure, draw_1, disease_id, iu_id, sim_no )
-VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s )
-'''
+from sch_simulation.helsim_RUN_KK import *
+from sch_simulation.helsim_FUNC_KK import *
 
 class MissingArgumentError( ValueError ):
     pass
 
-def run( runInfo, numSims, DB, useCloudStorage ):
+class DirectoryNotFoundError( ValueError ):
+    pass
 
-    DISEASE_CLOUD_PATH = f'diseases/{runInfo["type"]}-{runInfo["species"].lower()}/source-data'
+def sim_result_transform_generator( results, iu, species, scenario, numSims ):
 
-    iu = runInfo["iu_code"]
+    # espen_loc,year_id,age_start,age_end,sex_id,intensity,scenario,species,sequelae,measure
+    ihme_keys = {
+        "espen_loc":[],
+        "year_id":[],
+        "age_start":[],
+        "age_end":[],
+        "intensity":[],
+        "scenario":[],
+        "species":[],
+        "measure":[]
+    }
+
+    for i in range( 0, numSims ):
+        ihme_keys[ f'draw_{i}' ] = []
+
+    # we're going to take the draw_1 from each run result for an IU:
+
+    #   Time,age_start,age_end,intensity,species,measure,draw_1
+    #   0.0,0,1,light,Mansoni,prevalence,0.0
+
+    # and add it as draw_x at the end of a row in this output file:
+
+    #   espen_loc,year_id,age_start,age_end,intensity,scenario,species,measure,draw_0,draw_1,draw_3,draw_4,draw_x,draw_x,,
+    #   BFA05335,2030,4,4,light,1,mansoni,prevalence,0.1,0.08,0.09,0.11,,
+
+    ################################################################################
+    # IHME data file
+    ################################################################################
+
+    print( f'-> starting IHME transform for {numSims} simulations' )
+    a = time.time()
+
+    ihme_values = []
+
+    # first 7440 rows = standard ESPEN results + population data
+    for rowIndex, row in results[0][0:7440].iterrows():
+
+        if rowIndex > 0 and rowIndex % 1000 == 0:
+            print( f"-> {rowIndex}" )
+
+        # add the first fields up to the first draw
+        scenario_fields = [ iu, math.trunc( row.Time + 2018 ), row.age_start, row.age_end, row.intensity, scenario, species, row.measure, row.draw_1 ]
+        ihme_values.insert( rowIndex, scenario_fields )
+
+        # add draw_1 from the current line in each file as draw_X in the ihme file
+        for simNo in range(1,numSims):
+            ihme_values[ rowIndex ].append( results[ simNo ].loc[ rowIndex ].draw_1 )
+
+    b = time.time()
+    print( f'-> finished IHME transform for {numSims} simulations in {(b-a):.3f}s' )
+
+    yield pd.DataFrame( ihme_values, columns = ihme_keys )
+
+    ################################################################################
+    # IPM costs file
+    ################################################################################
+
+    print( f'-> starting IPM transform for {numSims} simulations' )
+    a = time.time()
+
+    ipm_values = []
+
+    # 7440 -> end = cost data
+    for rowIndex, row in results[0][7440:].iterrows():
+
+        arrIndex = rowIndex - 7440
+
+        if arrIndex > 0 and arrIndex % 1000 == 0:
+            print( f"-> {arrIndex}" )
+
+        # add the first fields up to the first draw
+        ipm_values.insert( arrIndex, [ iu, math.trunc( row.Time + 2018 ), row.age_start, row.age_end, row.intensity, scenario, species, row.measure, row.draw_1 ] )
+
+        # add draw_1 from the current line in each file as draw_X in the ipm file
+        for simNo in range(1,numSims):
+            ipm_values[ arrIndex ].append( results[ simNo ].loc[ arrIndex ].draw_1 )
+
+    b = time.time()
+    print( f'-> finished IPM transform for {numSims} simulations in {(b-a):.3f}s' )
+
+    yield pd.DataFrame( ipm_values, columns = ihme_keys )
+
+    return
+
+def run( runInfo, scenario, numSims, DB, useCloudStorage, saveResults=False ):
+
+    DISEASE_CLOUD_PATH = f'diseases/{runInfo[ "type" ]}-{runInfo[ "species" ].lower()}/source-data'
+
+    # make sure local data directory is present
+    if useCloudStorage == False:
+        if not os.path.isdir( "./local-data" ):
+            raise DirectoryNotFoundError( "./local-data" )
+
+    iu = runInfo[ "iu_code" ]
     region = iu[:3]
-    short = runInfo["short"]
+    short = runInfo[ "short" ]
+    species = runInfo[ "species" ]
 
-    InSimFilePath = f'/tmp/{short}_{iu}.p'
+    InSimFilePath = f'./local-data/{short}_{iu}.p'
     GcsInSimFilePath = f'{DISEASE_CLOUD_PATH}/{region}/{iu}/{short}_{iu}.p'
 
-    RkFilePath = f'/tmp/Input_Rk_{short}_{iu}.csv'
+    RkFilePath = f'./local-data/Input_Rk_{short}_{iu}.csv'
     GcsRkFilePath = f'gs://ntd-disease-simulator-data/{DISEASE_CLOUD_PATH}/{region}/{iu}/Input_Rk_{short}_{iu}.csv'
 
     coverageTextFileStorageName = f'/tmp/{short}_{iu}_MDA_vacc.txt'
@@ -42,46 +136,30 @@ def run( runInfo, numSims, DB, useCloudStorage ):
         cloudModule = gcs if useCloudStorage else None
     )
 
-    db_start_time = time.time()
+    scenario = 'SCEN_01'
+    transformer = sim_result_transform_generator( results, iu, runInfo['species'], scenario, numSims )
 
-    for sim_no in range( len( results ) ):
+    ihme_df = next( transformer )
+    ihme_file_name = f"ihme-{iu}-{runInfo['species']}-{scenario}-{numSims}.csv"
+    ihme_df.to_csv( ihme_file_name, index=False )
 
-        df = results[ sim_no ]
-        df.drop( columns = ['species'], inplace = True )
-        df[ 'disease_id' ] = runInfo[ 'disease_id' ]
-        df[ 'iu_id' ] = runInfo[ 'iu_id' ]
-        df[ 'sim_no' ] = sim_no
-        # print( df )
-        df_list = df.values
+    ipm_df = next( transformer )
+    ipm_file_name = f"ipm-{iu}-{runInfo['species']}-{scenario}-{numSims}.csv"
+    ipm_df.to_csv( ipm_file_name, index=False )
 
-        data = []
-        for sim_row in df.values:
-            data.append( tuple( sim_row ) )
+    if saveResults == True:
+        for i in range(len(results)):
+            df = results[i]
+            df.to_csv( f'{iu}_results_{i:03}.csv', index=False )
 
-        print( f"-> inserting {len(data)} results for sim_no {sim_no} ({runInfo['short']}:{runInfo['iu_code']}) into db" )
-
-        start_time = time.time()
-        DB.cursor().executemany( insert_result_sql, data )
-        DB.commit()
-        end_time = time.time()
-        total_time = end_time - start_time
-        print( f"  -> {total_time:.3f}s" )
-
-    db_end_time = time.time()
-    total_db_time = db_end_time - db_start_time
-    print( f"-> total time writing to db {total_db_time:.3f}s" )
-
+    return
 
 def run_model( InSimFilePath=None, RkFilePath=None, coverageFileName='Coverage_template.xlsx', coverageTextFileStorageName=None,
-                demogName='Default', paramFileName='sch_example.txt', resultOutputPath=None, numSims=None, cloudModule=None ):
+                demogName='Default', paramFileName='sch_example.txt', numSims=None, cloudModule=None ):
     '''
     File to load in a pickle file and associated parameters file and then
     run forward in time 23 years and give back results
-    Can store results as a set of csv's if saveResults = True
     '''
-
-    # flag for saving results or not
-    saveResults = ( resultOutputPath != None )
 
     # number of simulations to run
     if numSims is None:
@@ -143,8 +221,3 @@ def run_model( InSimFilePath=None, RkFilePath=None, coverageFileName='Coverage_t
     print( f'-> finished {numSims} simulations on {num_cores} cores in {(end_time - start_time):.3f}s' )
 
     return results
-
-#    if saveResults:
-#        for i in range(len(results)):
-#            df = results[i]
-#            df.to_csv(f'tmp/Man_AGO02049_results_{i:03}.csv', index=False)
