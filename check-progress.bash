15292df4fc16b167a5f371ff3f8c7db714fdd1a0
Igor Clark <igor@igorclark.net>
2022-10-02 20:37:34 +0100

    backup commit

--- check-progress.bash
+++ check-progress.bash
@@ -1,17 +1,45 @@
 #!/usr/bin/env bash
+
+echo "-------------------------------------------------------------------------------------------------------------"
+
 while true ; do
-    echo "------------------------------------------------------------------------------------------------"
-    for s in ascaris hookworm ; do
-        printf '%-9s| ' "${s}"
+
+#    for s in ascaris:1 hookworm:1 mansoni:4 whipworm:4 ; do
+    for s in ascaris:1 ; do
+        short=$(echo $s|cut -f 1 -d :)
+        zone=$(echo $s|cut -f 2 -d :)
+        printf '%-9s| ' "${short}"
         date | tr '\n' ' '
         echo -n '| '
-        gcloud compute ssh ntd-endgame-model-runner-$s-128 --project=artrabbit-clients-ntd --zone=europe-west1-b --command='ps uxf|grep "results/"|grep -vE "(grep|tail)"' | awk '{print $14}'
+        res=$(gcloud compute ssh ntd-endgame-model-runner-${short}-128 --project=artrabbit-clients-ntd --zone=europe-west${zone}-b --command='ps uxf|grep "results/"|grep -vE "(grep|tail)"' | awk '{print $14}' )
+        iu=$( echo $res | awk -F '-' '{print $3}' | sed 's/\.out$//g' )
+        line=$( ag $iu sch-sth-tra/iu-disease-data/mansoniIUs_scenario0.csv | cut -f 1 -d :| tr '\n' ' ' )
+        echo -n "$iu | "
+        printf '%-8s| ' "$line"
+        echo "$res"
     done
-    for s in mansoni whipworm ; do
-        printf '%-9s| ' "${s}"
-        date | tr '\n' ' '
-        echo -n '| '
-        gcloud compute ssh ntd-endgame-model-runner-$s-128 --project=artrabbit-clients-ntd --zone=europe-west4-b --command='ps uxf|grep "results/"|grep -vE "(grep|tail)"' | awk '{print $14}'
-    done
+
     sleep 180
+    continue
+
+#    for z in north1 west2 west8 west9 ; do
+    for z in north1 ; do
+        for n in a b ; do
+            host="${z}b-${n}"
+            zone="${z}-b"
+            printf '%-9s| ' "${host}"
+            date | tr '\n' ' '
+            echo -n '| '
+            res=$(gcloud compute ssh ntd-endgame-model-runner-mansoni-${host}-128 --project=artrabbit-clients-ntd --zone=europe-${zone} --command='ps uxf|grep "results/"|grep -vE "(grep|tail)"' | awk '{print $14}')
+            iu=$( echo $res | awk -F '-' '{print $3}' | sed 's/\.out$//g' )
+            line=$( ag $iu sch-sth-tra/iu-disease-data/mansoniIUs_scenario0.csv | cut -f 1 -d : | tr '\n' ' ' )
+            echo -n "$iu | "
+            printf '%-8s| ' "$line"
+            echo "$res"
+        done
+    done
+
+    echo "-------------------------------------------------------------------------------------------------------------"
+    sleep 180
+
 done
