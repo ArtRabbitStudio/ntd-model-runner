65c4ee9aec435ddd905d33a2cbdd43069563c165
Igor Clark <igor@igorclark.net>
2022-10-10 16:17:29 +0100

    model runner can auto-split IUs into X/Y segments

--- sch-sth-tra/run-sch-sth-tra.bash
+++ sch-sth-tra/run-sch-sth-tra.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # default values
 uncompressed=""
 local_storage=""
@@ -19,9 +21,10 @@ DISEASE_SHORT_NAMES_TO_CODES[Man]="sch-mansoni"
 
 function usage() {
     echo "usage: ${0}"
-    echo "            -d <short-disease> -s <scenario-list> -i <iu-list-file> -n <num-sims> -o <output-folder>"
+    echo "            -d <short-disease-code> -s <scenario-list> -i <iu-list-file> -n <num-sims> -o <output-folder>"
     echo "            [-p <source-data-path>] [-u (uncompressed)] [-l (local_storage)]"
     echo "            [-r <read-pickle-file-suffix>] [-f <save-pickle-file-suffix>] [-b <burn-in-years>]"
+    echo "            [-g <segment-number,out-of> ]"
     exit 1
 }
 
@@ -29,7 +32,7 @@ function run_scenarios () {
 
     for scenario in ${scenarios//,/ } ; do
 
-        lines=$( grep -v IU_ID ${iu_list_file} | sed 's/"//g' )
+        lines=$( get_lines ${segment_number:=} ${out_of:=} ${iu_list_file} )
 
         for line in $lines ; do
 
@@ -68,7 +71,7 @@ function run_scenarios () {
                 save_pickle_cmd="-f ${save_pickle_file_suffix}"
             fi
 
-            if [[ -z "${burn_in_time}" ]] ; then
+            if [[ -z "${burn_in_time:=}" ]] ; then
                 burn_in_time_cmd=""
             else
                 burn_in_time_cmd="-b ${burn_in_time}"
@@ -86,6 +89,41 @@ function run_scenarios () {
         done
 
     done
+
+}
+
+function get_lines () {
+
+    # if there's no segment_number/out_of variables then just strip & output all the lines from the file
+    if [[ -z ${1:-} || -z ${2:-} ]] ; then
+        grep -v IU_ID ${iu_list_file} | sed 's/"//g'
+        return
+    fi
+
+    # otherwise work out the appropriate segment of lines to use
+    local seg=$1
+    local requested_segments=$2
+    local file=$3
+
+    local line_count=$(( $( wc -l < ${file} | awk '{print $1}' ) - 1 ))
+
+    local num_per_segment=$(( $line_count / $requested_segments ))
+    local remainder=$(( $line_count % ( num_per_segment * $requested_segments )  ))
+    local total_from_segments=$(( $requested_segments * $num_per_segment ))
+    echo "-> $requested_segments segments * $num_per_segment lines makes $total_from_segments lines total, leaving a remainder of ${remainder} from a ${line_count}-line file, using a last segment of $(( num_per_segment + remainder )) to make a total of $(( $total_from_segments + $remainder )) " >&2
+
+    local first_line=$(( $num_per_segment * ( $seg - 1 ) + 1 ))
+    local last_line=$(( ( $first_line + $num_per_segment ) - 1 ))
+
+    if [[ ${seg} = ${requested_segments} ]] ; then
+        last_line=$(( $last_line + $remainder ))
+    fi
+    local end_line=$(( $last_line + 1 ))
+
+    local segment_line_count=$(( ( last_line - first_line ) + 1 ))
+    echo "-> segment number ${seg} has ${segment_line_count} lines, from $first_line - $last_line inclusive" >&2
+
+    grep -v IU_ID ${iu_list_file} | sed 's/"//g' | sed -n "${first_line},${last_line}p;${end_line}q"
 
 }
 
@@ -162,7 +200,7 @@ function maybe_fetch_files () {
 }
 
 # work out disease, num sims, output folder, file options
-while getopts ":d:s:i:n:o:p:r:f:b:ulh" opts ; do
+while getopts ":d:s:i:n:o:p:r:f:g:b:ulh" opts ; do
 
     case "${opts}" in
 
@@ -204,6 +242,26 @@ while getopts ":d:s:i:n:o:p:r:f:b:ulh" opts ; do
             fi
             ;;
 
+        g)
+            segment_number_out_of=${OPTARG}
+            if [[ -z "$( echo ${segment_number_out_of} | grep , )" ]] ; then
+                usage
+            fi
+
+            segment_number=$( echo ${segment_number_out_of} | cut -f 1 -d , )
+            out_of=$( echo ${segment_number_out_of} | cut -f 2 -d , )
+
+            if [[ ${segment_number} -le 0 || ${out_of} -le 0 ]] ; then
+                echo "error: segment number/total number of segments are invalid" >&2
+                usage
+            fi
+
+            if [[ ${segment_number} -gt ${out_of} ]] ; then
+                echo "error: segment number ${segment_number} is larger than total number of segments ${out_of}" >&2
+                usage
+            fi
+            ;;
+
         b)
             burn_in_time=${OPTARG}
             if [[ "${burn_in_time:0:1}" == "-" ]] ; then
@@ -231,20 +289,21 @@ while getopts ":d:s:i:n:o:p:r:f:b:ulh" opts ; do
 
 done
 
-if [[ -z "${disease}" || -z "${num_sims}" || -z "${output_folder}" || ! -f "${iu_list_file}" ]] ; then
+if [[ -z "${disease:=}" || -z "${num_sims:=}" || -z "${output_folder:=}" || ! -f "${iu_list_file:=}" ]] ; then
     usage
 fi
 
-if [[ "${DISPLAY_CMD:=n}" != "y" ]] ; then
-    mkdir -p $result_folder
+if [[ "${DISPLAY_CMD:=n}" == "y" ]] ; then
+    echo "making folder"
+    #mkdir -p $result_folder
 fi
 
-if [[ "${save_pickle_file_suffix}" = "${read_pickle_file_suffix}" ]] ; then
+if [[ "${save_pickle_file_suffix:=}" = "${read_pickle_file_suffix:=}" ]] && [[ -n "${read_pickle_file_suffix:=}" ]]; then
     echo "error: output pickle file suffix must be different from input pickle file suffix" >&2
     usage
 fi
 
-if [[ -n "${save_pickle_file_suffix}" && -z "${burn_in_time}" ]] ; then
+if [[ -n "${save_pickle_file_suffix:=}" && -z "${burn_in_time:=}" ]] ; then
     echo "error: burn-in requires a number of years" >&2
     usage
 fi
