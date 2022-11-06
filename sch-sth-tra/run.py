7203fedc3ff8aebeaf58326d6e408b3aefe8dd41
Igor Clark <igor@igorclark.net>
2022-11-06 18:02:07 +0000

    organise main python run script functions

--- sch-sth-tra/run.py
+++ sch-sth-tra/run.py
@@ -2,127 +2,147 @@ import sys
 import signal
 
 from optparse import OptionParser
+from types import SimpleNamespace
 
 # local imports
 from ntd_model_runner import run, DirectoryNotFoundError, MissingArgumentError
 from db import db
 
+# SQL statements
+# local functions
 def iu_list_callback( option, opt, value, parser ):
     setattr( parser.values, option.dest, value.split( ',' ) )
 
 # get CLI options
-parser = OptionParser()
+def get_cli_options():
+    parser = OptionParser()
 
-parser.add_option( '-o', '--output-folder', dest='outputFolder', default='202206' )
-parser.add_option( '-d', '--disease', dest='disease', default='Man' )
-parser.add_option( '-m', '--demography-name', dest='demogName', default="Default" )
-parser.add_option( '-i', '--iu-list', dest='iuList', type='string', action='callback', callback=iu_list_callback, default='' )
-parser.add_option( '-n', '--num-sims', dest='numSims', default=1 )
-parser.add_option( '-l', '--local-storage', action='store_false', dest='useCloudStorage', default=True )
-parser.add_option( '-s', '--scenario', dest='scenario', type='string', default='1' )
-parser.add_option( '-g', '--group-id', dest='groupId', type='int', default=None )
-parser.add_option( '-u', '--uncompressed-output', dest='compress', action='store_false', default=True )
-parser.add_option( '-p', '--source-data-path', dest='sourceDataPath', default='source-data' )
-parser.add_option( '-r', '--read-pickle-file-suffix', dest='readPickleFileSuffix', type='string', default=None )
-parser.add_option( '-f', '--save-pickle-file-suffix', dest='savePickleFileSuffix', type='string', default=None )
-parser.add_option( '-b', '--burn-in-time', dest='burnInTime', type='int', default=None )
+    parser.add_option( '-o', '--output-folder', dest='outputFolder', default='202206' )
+    parser.add_option( '-d', '--disease', dest='disease', default='Man' )
+    parser.add_option( '-m', '--demography-name', dest='demogName', default="Default" )
+    parser.add_option( '-i', '--iu-list', dest='iuList', type='string', action='callback', callback=iu_list_callback, default='' )
+    parser.add_option( '-n', '--num-sims', dest='numSims', default=1 )
+    parser.add_option( '-l', '--local-storage', action='store_false', dest='useCloudStorage', default=True )
+    parser.add_option( '-s', '--scenario', dest='scenario', type='string', default='1' )
+    parser.add_option( '-g', '--group-id', dest='groupId', type='int', default=None )
+    parser.add_option( '-u', '--uncompressed-output', dest='compress', action='store_false', default=True )
+    parser.add_option( '-p', '--source-data-path', dest='sourceDataPath', default='source-data' )
+    parser.add_option( '-r', '--read-pickle-file-suffix', dest='readPickleFileSuffix', type='string', default=None )
+    parser.add_option( '-f', '--save-pickle-file-suffix', dest='savePickleFileSuffix', type='string', default=None )
+    parser.add_option( '-b', '--burn-in-time', dest='burnInTime', type='int', default=None )
 
-( options, args ) = parser.parse_args()
+    ( options, args ) = parser.parse_args()
 
-iuList = options.iuList if isinstance( options.iuList, list ) == True else [ '' ]
-numSims = int( options.numSims )
-disease = options.disease
-demogName = options.demogName
-useCloudStorage = options.useCloudStorage
-outputFolder = options.outputFolder
-groupId = options.groupId if options.groupId != 0 else None
-scenario = options.scenario
-compress = options.compress
-sourceDataPath = options.sourceDataPath
-readPickleFileSuffix = options.readPickleFileSuffix
-savePickleFileSuffix = options.savePickleFileSuffix
-burnInTime = options.burnInTime
+    return options
 
-if readPickleFileSuffix == savePickleFileSuffix and readPickleFileSuffix != None:
-    print( "-> pickle output destination must be different from pickle input destination." )
-    sys.exit( 1 )
+# check the run info in the DB for the requested list of IUs
+def get_run_info( DB, run_options ):
 
-cli_iu_list_len = len( [ x for x in iuList if x != '' ] )
-cloudStorageStr = '' if useCloudStorage else 'not '
-compressStr = '' if compress else 'not '
-print( f"-> {numSims} simulations for {cli_iu_list_len} IUs requested, {cloudStorageStr}using cloud storage, {compressStr}compressing output files" )
-if outputFolder != '':
-    print (f"-> saving result to output folder {outputFolder}" )
+    format_strings = ', '.join( [ '%s' ] * len( run_options.iuList ) )
 
-# instantiate local db module
-DB = db()
+    runs_sql = '''
+    SELECT d.type, d.species, d.short, i.code AS iu_code, d.id AS disease_id, i.id AS iu_id
+    FROM disease d, iu i, iu_disease i_d
+    WHERE i.id = i_d.iu_id and d.id = i_d.disease_id
+    AND i.code IN ( %s )''' % format_strings
 
-# handle exits gracefully
-def handler( signum, frame ):
-    DB = None
-    sys.exit()
+    runs_sql += '''
+    AND d.short = %s
+    ORDER BY d.type, d.species, i.code
+    '''
 
-# register ctrl-c handler
-signal.signal( signal.SIGINT, handler )
+    disease_iu_params = tuple( run_options.iuList ) + tuple( [ run_options.disease ] )
+    DB.query( runs_sql, disease_iu_params )
 
-# sql statements
-format_strings = ', '.join( [ '%s' ] * len( iuList ) )
-runs_sql = '''
-SELECT d.type, d.species, d.short, i.code AS iu_code, d.id AS disease_id, i.id AS iu_id
-FROM disease d, iu i, iu_disease i_d
-WHERE i.id = i_d.iu_id and d.id = i_d.disease_id
-AND i.code IN ( %s )''' % format_strings
-
-runs_sql += '''
-AND d.short = %s
-ORDER BY d.type, d.species, i.code
-'''
-
-# get the runs
-disease_iu_params = tuple( iuList ) + tuple( [ disease ] )
-disease_iu_combos = DB.query( runs_sql, disease_iu_params, )
-print( f'-> found {DB.cursor().rowcount} disease/IU combos' )
-
-# track current run
-last_iu_combo = None
+    print( f'-> found {DB.cursor().rowcount} disease/IU combos' )
 
 # run the runs
-runs = []
-for( runInfo ) in DB.cursor():
-    runs.append( runInfo )
+def carry_out_runs( DB, run_options ):
 
-for runInfo in runs:
-    d_id = runInfo[ 'disease_id' ]
-    i_id = runInfo[ 'iu_id' ]
+    for( runInfo ) in DB.cursor():
 
-    runInfo[ 'demogName' ] = demogName
+    #    print( f"run( {runInfo}, {groupId}, {scenario}, {numSims}, {DB}, {useCloudStorage}, {compress}, {False}, {outputFolder}" );
+    #    sys.exit(0)
 
-#    print( f"run( {runInfo}, {groupId}, {scenario}, {numSims}, {DB}, {useCloudStorage}, {compress}, {False}, {outputFolder}" );
-#    sys.exit(0)
+        runInfo[ 'demogName' ] = run_options.demogName
 
-    try:
-        print( f"-> running {numSims} simulations for {runInfo['short']}:{runInfo['iu_code']}" )
-        run(
-            runInfo = runInfo,
-            groupId = groupId,
-            scenario = scenario,
-            numSims = numSims,
-            DB = DB,
-            useCloudStorage = useCloudStorage,
-            compress = compress,
-            readPickleFileSuffix=readPickleFileSuffix,
-            savePickleFileSuffix=savePickleFileSuffix,
-            burnInTime = burnInTime,
-            saveIntermediateResults=False,
-            outputFolder = outputFolder,
-            sourceDataPath = sourceDataPath
-        )
+        try:
+            print( f"-> running {run_options.numSims} simulations for {runInfo['short']}:{runInfo['iu_code']}" )
+            run(
+                runInfo = runInfo,
+                groupId = run_options.groupId,
+                scenario = run_options.scenario,
+                numSims = run_options.numSims,
+                DB = DB,
+                useCloudStorage = run_options.useCloudStorage,
+                compress = run_options.compress,
+                readPickleFileSuffix=run_options.readPickleFileSuffix,
+                savePickleFileSuffix=run_options.savePickleFileSuffix,
+                burnInTime = run_options.burnInTime,
+                saveIntermediateResults=False,
+                outputFolder = run_options.outputFolder,
+                sourceDataPath = run_options.sourceDataPath
+            )
 
-    except DirectoryNotFoundError as d:
-        print( f"xx> local data directory not found: {d}" )
+        except DirectoryNotFoundError as d:
+            print( f"xx> local data directory not found: {d}" )
 
-    except FileNotFoundError as f:
-        print( f"xx> file not found: {f}" )
+        except FileNotFoundError as f:
+            print( f"xx> file not found: {f}" )
 
-    except MissingArgumentError as m:
-        print( f"xx> argument not provided: {m}" )
+        except MissingArgumentError as m:
+            print( f"xx> argument not provided: {m}" )
+
+# main code block
+if __name__ == '__main__':
+
+    options = get_cli_options()
+
+    # work out what to do with the options
+    run_options = SimpleNamespace( **{
+        'iuList': options.iuList if isinstance( options.iuList, list ) == True else [ '' ],
+        'numSims': int( options.numSims ),
+        'disease': options.disease,
+        'demogName': options.demogName,
+        'useCloudStorage': options.useCloudStorage,
+        'outputFolder': options.outputFolder,
+        'groupId': options.groupId if options.groupId != 0 else None,
+        'scenario': options.scenario,
+        'compress': options.compress,
+        'sourceDataPath': options.sourceDataPath,
+        'readPickleFileSuffix': options.readPickleFileSuffix,
+        'savePickleFileSuffix': options.savePickleFileSuffix,
+        'burnInTime': options.burnInTime,
+    } )
+
+    # check pickle options are OK
+    if run_options.readPickleFileSuffix == run_options.savePickleFileSuffix and run_options.readPickleFileSuffix != None:
+        print( "-> pickle output destination must be different from pickle input destination." )
+        sys.exit( 1 )
+
+    # display run info
+    cli_iu_list_len = len( [ x for x in run_options.iuList if x != '' ] )
+    cloudStorageStr = '' if run_options.useCloudStorage else 'not '
+    compressStr = '' if run_options.compress else 'not '
+
+    print( f"-> {run_options.numSims} simulations for {cli_iu_list_len} IUs requested, {cloudStorageStr}using cloud storage, {compressStr}compressing output files" )
+
+    if run_options.outputFolder != '':
+        print (f"-> saving result to output folder {run_options.outputFolder}" )
+
+    # instantiate local db module
+    DB = db()
+
+    # handle exits gracefully
+    def handler( signum, frame ):
+        DB = None
+        sys.exit()
+
+    # register ctrl-c handler
+    signal.signal( signal.SIGINT, handler )
+
+    # check the work is valid
+    get_run_info( DB, run_options )
+
+    # do the work
+    carry_out_runs( DB, run_options )
