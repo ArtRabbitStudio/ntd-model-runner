16bf140fef91fb4e24f69d99212a9be03681e3da
Igor Clark <igor@igorclark.net>
2023-07-28 17:01:20 +0100

    all rwanda diseases save to same run folder

--- sch-sth-tra/kick-off-multi-scenario-runs-rwanda-202307.bash
+++ sch-sth-tra/kick-off-multi-scenario-runs-rwanda-202307.bash
@@ -8,7 +8,7 @@ dry_run=${DRY_RUN:-n}
 for file in iu-disease-data/rwanda-202307/${disease_long}-IUs-*.csv ; do
     echo $file
     scenario=$( basename $file | cut -f 4 -d '-' | cut -f 1 -d . )
-    DISPLAY_CMD=${dry_run} FORCE_ADD_TO_EXISTING_RUN=y DONT_WRITE_DB_RECORD=y bash run-sch-sth-tra.bash -s ${scenario} -i ${file} -n ${num_sims} -d ${disease_short} -p source-data-rwanda-fitting-202307a -k ntd-disease-simulator-data -K ${output_bucket} -N "202307d-${disease_long}-rwanda" -o 202307d-${disease_long}-rwanda -e igor@artrabbit.com
+    DISPLAY_CMD=${dry_run} FORCE_ADD_TO_EXISTING_RUN=y DONT_WRITE_DB_RECORD=y bash run-sch-sth-tra.bash -s ${scenario} -i ${file} -n ${num_sims} -d ${disease_short} -p source-data-rwanda-fitting-202307a -k ntd-disease-simulator-data -K ${output_bucket} -N "202307d-rwanda" -o 202307d-rwanda -e igor@artrabbit.com
 done
 
 
