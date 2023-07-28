7e8f732dc7cac8b31468af67993ed4a317e59fc7
Igor Clark <igor@igorclark.net>
2023-07-28 17:39:49 +0100

    use year 2020.5 for rwanda

--- sch-sth-tra/ntd_model_runner.py
+++ sch-sth-tra/ntd_model_runner.py
@@ -506,6 +506,8 @@ def transform_results_with_pandas( results, iu, type, species, scenario, numSims
         output[key] = results[ 0] [ key ][ startrow:endrow ]
 
     # work out the year for the row
+    #output[ 'year_id' ] = ( results[ 0 ][ 'Time' ] + 2018 ).astype( int )
+    ## use 2020.5 for Rwanda TODO FIXME revert for Endgame
     output[ 'year_id' ] = ( results[ 0 ][ 'Time' ] + 2018 ).astype( int )
 
     # copy the static rows into the full list
