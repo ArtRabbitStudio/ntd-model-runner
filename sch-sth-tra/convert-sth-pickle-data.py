4bb7abbf57b24d040e6e76621a6754405df52551
Igor Clark <igor@igorclark.net>
2024-05-09 16:05:32 -0400

    update STH data converters for whipworm+roundworm

--- sch-sth-tra/convert-sth-pickle-data.py
+++ sch-sth-tra/convert-sth-pickle-data.py
@@ -6,10 +6,20 @@ from gcs import gcs
 data_bucket = "ntd-disease-simulator-data"
 GCS = gcs( data_bucket )
 
-iu = sys.argv[ 1 ]
-iu_path = f"{iu[:3]}/{iu}/Hook_{iu}.p"
+lowercase_to_short_uppercase_disease_names = {
+    'hookworm': 'Hook',
+    'whipworm': 'Tri',
+    'roundworm': 'Asc'
+}
 
-source_path = "diseases/sth-hookworm/source-data-uploaded-20220712"
+disease = sys.argv[ 1 ]
+short_disease = lowercase_to_short_uppercase_disease_names[ disease ]
+iu = sys.argv[ 2 ]
+iu_path = f"{iu[:3]}/{iu}/{short_disease}_{iu}.p"
+source_data_dir = sys.argv[ 3 ]
+converted_data_dir= sys.argv[ 4 ]
+
+source_path = f"diseases/sth-{disease}/{source_data_dir}"
 pickle_data = pickle.loads( GCS.get_blob( f"{source_path}/{iu_path}" ) )
 
 row_list = []
@@ -19,6 +29,6 @@ for i in range( len( pickle_data ) ):
 
 print( f"=> converted {len( row_list )} entries" )
 
-output_path = "diseases/sth-hookworm/source-data-converted-20240505"
+output_path = f"diseases/sth-{disease}/{converted_data_dir}"
 GCS.write_string_to_file( pickle.dumps( row_list, protocol=pickle.HIGHEST_PROTOCOL ), f"{output_path}/{iu_path}" )
 print( "=> done." )
--- sch-sth-tra/convert-sth-pickle-data.sh
+++ sch-sth-tra/convert-sth-pickle-data.sh
@@ -1,7 +1,21 @@
-IU=${1}
-echo python convert-sth-pickle-data.py ${IU}
-for s in $( gsutil ls gs://ntd-disease-simulator-data/diseases/sth-hookworm/source-data-uploaded-20220712/${IU:0:3}/${IU}/*.csv ) ; do
+DISEASE=${1}
+IU=${2}
+SOURCE_DATA_DIR=${3:-source-data}
+CONVERTED_SOURCE_DATA_DIR="source-data-converted-$( date +%Y%m%d )"
+
+python convert-sth-pickle-data.py ${DISEASE} ${IU} ${SOURCE_DATA_DIR} ${CONVERTED_SOURCE_DATA_DIR}
+
+glob_path="gs://ntd-disease-simulator-data/diseases/sth-${DISEASE}/${SOURCE_DATA_DIR}/${IU:0:3}/${IU}"
+glob="gs://ntd-disease-simulator-data/diseases/sth-${DISEASE}/${SOURCE_DATA_DIR}/${IU:0:3}/${IU}"
+
+for s in $( gsutil ls ${glob} ) ; do
     f=$( echo $s | awk -F / '{print $NF}' )
-    echo "--> $f"
-    gsutil cp ${s} ${s/uploaded-20220712/converted-20240505}
+    newname=$( echo $s | sed -e "s/${SOURCE_DATA_DIR}/${CONVERTED_SOURCE_DATA_DIR}/" )
+    echo "  --> from: $f"
+    echo "    --> to: $newname"
+    gsutil cp ${s} "${newname}"
 done
+
+echo "==> new converted directory for ${IU}:"
+new_glob_path="gs://ntd-disease-simulator-data/diseases/sth-${DISEASE}/${CONVERTED_SOURCE_DATA_DIR}/${IU:0:3}/${IU}"
+gsutil ls -l "${new_glob_path}/*"
