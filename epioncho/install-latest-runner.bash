050cfc867d320c772d60385f549f9a1c3ea0377b
Igor Clark <igor@igorclark.net>
2025-02-18 23:15:03 -0800

    add epioncho install-latest-runner script

---
+++ epioncho/install-latest-runner.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+cd
+cd ntd-model-runner/epioncho
+git fetch
+git pull origin feature/endgame-result-importer
+bash setup.bash
