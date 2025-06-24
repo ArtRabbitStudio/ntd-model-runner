328447f71339bf84c9357cd8022f86f128532677
Igor Clark <igor@igorclark.net>
2025-06-24 13:01:17 +0100

    peg setuptools==78.1.0 in epioncho setup

--- epioncho/setup.bash
+++ epioncho/setup.bash
@@ -36,7 +36,7 @@ pipenv --python 3.10.9
 pipenv run curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 pipenv run python get-pip.py --force-reinstall
 pipenv run rm get-pip.py
-pipenv run pip install -U setuptools
+pipenv run pip install -U setuptools==78.1.0
 pipenv run pip install .
 echo
 
